70c8b69d7637a6bb95e038fe8e080db2b7c87c95
Thomas ROUSSELIN <t.rousselin@mtarget.fr>
2025-05-14 13:51:47 +0200

    Save a new student

---
+++ src/main/java/com/devattom/SpringHibernateJpa/DAO/StudentDAO.java
@@ -0,0 +1,8 @@
+package com.devattom.SpringHibernateJpa.DAO;
+
+import com.devattom.SpringHibernateJpa.entity.Student;
+
+public interface StudentDAO {
+
+    void save(Student student);
+}
--- src/main/java/com/devattom/SpringHibernateJpa/SpringHibernateJpaApplication.java
+++ src/main/java/com/devattom/SpringHibernateJpa/SpringHibernateJpaApplication.java
@@ -1,9 +1,10 @@
 package com.devattom.SpringHibernateJpa;
 
+import com.devattom.SpringHibernateJpa.DAO.StudentDAO;
+import com.devattom.SpringHibernateJpa.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
@@ -14,9 +15,17 @@ public class SpringHibernateJpaApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
-		return runner -> {
-			System.out.println("Welcome to SpringHibernateJpa Application");
-		};
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
+		return runner -> createStudent(studentDAO);
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+		System.out.println("Creating student...");
+		Student student = new Student("Tom", "Devattom", "t.rousselin@test.com");
+
+		System.out.println("Saving the student...");
+		studentDAO.save(student);
+
+		System.out.println("Saved student with id: " + student.getId());
 	}
 }
