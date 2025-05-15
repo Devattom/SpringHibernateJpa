f79162d45ae665ff19bcd9401c6085dd873fe259
Thomas ROUSSELIN <t.rousselin@mtarget.fr>
2025-05-15 13:05:16 +0200

    read a student

--- src/main/java/com/devattom/SpringHibernateJpa/SpringHibernateJpaApplication.java
+++ src/main/java/com/devattom/SpringHibernateJpa/SpringHibernateJpaApplication.java
@@ -16,16 +16,29 @@ public class SpringHibernateJpaApplication {
 
 	@Bean
 	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
-		return runner -> createStudent(studentDAO);
+		return runner -> {
+//			createStudent(studentDAO);
+			readStudent(studentDAO);
+		};
 	}
 
 	private void createStudent(StudentDAO studentDAO) {
 		System.out.println("Creating student...");
-		Student student = new Student("Tom", "Devattom", "t.rousselin@test.com");
+		Student student1 = new Student("Tom", "Devattom", "t.rousselin@test.com");
+		Student student2 = new Student("Luz", "Otero", "l.otero@test.com");
+		Student student3 = new Student("T", "test", "t.test@test.com");
 
 		System.out.println("Saving the student...");
-		studentDAO.save(student);
+		studentDAO.save(student1);
+		studentDAO.save(student2);
+		studentDAO.save(student3);
 
-		System.out.println("Saved student with id: " + student.getId());
+		System.out.println("Saved student with id: " + student1.getId());
+		System.out.println("Saved student with id: " + student2.getId());
+		System.out.println("Saved student with id: " + student3.getId());
+	}
+
+	private void readStudent(StudentDAO studentDAO) {
+		System.out.println(studentDAO.findById(3));
 	}
 }
