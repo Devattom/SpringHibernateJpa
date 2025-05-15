f1787bb83e890d8ea941566afcdc0e368652d5f3
Rousselin Thomas <98778784+Devattom@users.noreply.github.com>
2025-05-15 13:24:15 +0200

    add jpa queries (#6)
    Co-authored-by: Thomas ROUSSELIN <t.rousselin@mtarget.fr>

--- src/main/java/com/devattom/SpringHibernateJpa/DAO/StudentDAO.java
+++ src/main/java/com/devattom/SpringHibernateJpa/DAO/StudentDAO.java
@@ -2,9 +2,15 @@ package com.devattom.SpringHibernateJpa.DAO;
 
 import com.devattom.SpringHibernateJpa.entity.Student;
 
+import java.util.List;
+
 public interface StudentDAO {
 
     void save(Student student);
 
     Student findById(int id);
+
+    List<Student> findAll();
+
+    List<Student> findByLastName(String lastName);
 }
--- src/main/java/com/devattom/SpringHibernateJpa/DAO/StudentDAOImpl.java
+++ src/main/java/com/devattom/SpringHibernateJpa/DAO/StudentDAOImpl.java
@@ -2,9 +2,12 @@ package com.devattom.SpringHibernateJpa.DAO;
 
 import com.devattom.SpringHibernateJpa.entity.Student;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public class StudentDAOImpl implements StudentDAO{
@@ -25,5 +28,24 @@ public class StudentDAOImpl implements StudentDAO{
     @Override
     public Student findById(int id) {
         return entityManager.find(Student.class, id);
+    }
+
+    @Override
+    public List<Student> findAll() {
+        TypedQuery<Student> query =  entityManager.createQuery(
+                "SELECT s FROM Student s ORDER BY firstName"
+                , Student.class
+        );
+        return query.getResultList();
+    }
+
+    @Override
+    public List<Student> findByLastName(String lastName) {
+        TypedQuery<Student> query =  entityManager.createQuery(
+                "SELECT s FROM Student s WHERE s.lastName = :lastName",
+                Student.class
+        );
+        query.setParameter("lastName", lastName);
+        return query.getResultList();
     }
 }
